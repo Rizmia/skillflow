7f3d28091418d0f48d5262d8a73117fc7915f5a8
hashijayarathna <147933342+hashijayarathna@users.noreply.github.com>
2025-05-06 15:18:12 +0530

    Add MongoConfiguration java file

---
+++ backend/src/main/java/backend/backend/config/MongoConfiguration.java
@@ -0,0 +1,14 @@
+package backend.backend.config;
+
+//import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+// import com.mongodb.client.MongoClient;
+// import com.mongodb.client.MongoClients;
+
+@Configuration
+public class MongoConfiguration  {
+
+    
+}
