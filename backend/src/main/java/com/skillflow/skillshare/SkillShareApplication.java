23733f88ad622562e12e6ea9fe1efc2373900f06
Rizmia Mansoor <IT22883834@my.sliit.lk>
2025-05-07 22:31:57 +0530

    add Jwt Secret to .env file

--- backend/src/main/java/com/skillflow/skillshare/SkillShareApplication.java
+++ backend/src/main/java/com/skillflow/skillshare/SkillShareApplication.java
@@ -12,6 +12,7 @@ public class SkillShareApplication {
 		Dotenv dotenv = Dotenv.load();
 		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
 		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
 
 		SpringApplication.run(SkillShareApplication.class, args);
 	}
